b58506aae81268ebee68feacee17854abe8a9e3f
sᴛᴀʀ ʙᴏɪ <119308395+its-star-boi@users.noreply.github.com>
2023-02-04 11:17:35 +0530

    Update __init__.py

--- SpamX/__init__.py
+++ SpamX/__init__.py
@@ -1,5 +1,5 @@
 # Star boi - Telegram Projects
-# (c) 2022 - 2023 Star
+# (c) 2022 - 2024 Star
 # Don't Kang Bitch -! 
 
 
@@ -69,7 +69,7 @@ SUDO_USERS.append(OWNER_ID)
 # SUDO_USERS = list(filter(lambda x: x, map(int, os.getenv("SUDO_USERS", "5463205082").split())))
 #----------------------------------------------
 
-RiZoeL = Client(name="SESSION", api_id = API_ID, api_hash = API_HASH, session_string=SESSION, plugins=dict(root="SpamX.module"))
+StarX = Client(name="SESSION", api_id = API_ID, api_hash = API_HASH, session_string=SESSION, plugins=dict(root="SpamX.module"))
 print("Client 1 Found")
 
 
@@ -79,115 +79,115 @@ start_time = time.time()
 #-------------------------CLIENTS-----------------------------
 
 if SESSION2:
-    RiZoeL2 = Client(name="SESSION2", api_id = API_ID, api_hash = API_HASH, session_string=SESSION2, plugins=dict(root="SpamX.module"))
+    StarX2 = Client(name="SESSION2", api_id = API_ID, api_hash = API_HASH, session_string=SESSION2, plugins=dict(root="SpamX.module"))
     print("Client 2 Found")
 else:
-    RiZoeL2 = None
+    StarX2 = None
 
 if SESSION3:
-    RiZoeL3 = Client(name="SESSION3", api_id = API_ID, api_hash = API_HASH, session_string=SESSION3, plugins=dict(root="SpamX.module"))
+    StarX3 = Client(name="SESSION3", api_id = API_ID, api_hash = API_HASH, session_string=SESSION3, plugins=dict(root="SpamX.module"))
     print("Client 3 Found")
 else:
-    RiZoeL3 = None
+    StarX3 = None
 
 if SESSION4:
-    RiZoeL4 = Client(name="SESSION4", api_id = API_ID, api_hash = API_HASH, session_string=SESSION4, plugins=dict(root="SpamX.module"))
+    StarX4 = Client(name="SESSION4", api_id = API_ID, api_hash = API_HASH, session_string=SESSION4, plugins=dict(root="SpamX.module"))
     print("Client 4 Found")
 else:
-    RiZoeL4 = None
+    StarX4 = None
 
 if SESSION5:
-    RiZoeL5 = Client(name="SESSION5", api_id = API_ID, api_hash = API_HASH, session_string=SESSION5, plugins=dict(root="SpamX.module"))
+    StarX5 = Client(name="SESSION5", api_id = API_ID, api_hash = API_HASH, session_string=SESSION5, plugins=dict(root="SpamX.module"))
     print("Client 5 Found")
 else:
-    RiZoeL5 = None
+    StarX5 = None
 
 if SESSION6:
-    RiZoeL6 = Client(name="SESSION6", api_id = API_ID, api_hash = API_HASH, session_string=SESSION6, plugins=dict(root="SpamX.module"))
+    StarX6 = Client(name="SESSION6", api_id = API_ID, api_hash = API_HASH, session_string=SESSION6, plugins=dict(root="SpamX.module"))
     print("Client 6 Found")
 else:
-    RiZoeL6 = None
+    StarX6 = None
         
 if SESSION7:
-    RiZoeL7 = Client(name="SESSION7", api_id = API_ID, api_hash = API_HASH, session_string=SESSION7, plugins=dict(root="SpamX.module"))
+    StarX7 = Client(name="SESSION7", api_id = API_ID, api_hash = API_HASH, session_string=SESSION7, plugins=dict(root="SpamX.module"))
     print("Client 7 Found")
 else:
-    RiZoeL7 = None
+    StarX7 = None
 
 if SESSION8:
-    RiZoeL8 = Client(name="SESSION8", api_id = API_ID, api_hash = API_HASH, session_string=SESSION9, plugins=dict(root="SpamX.module"))
+    StarX8 = Client(name="SESSION8", api_id = API_ID, api_hash = API_HASH, session_string=SESSION9, plugins=dict(root="SpamX.module"))
     print("Client 8 Found")
 else:
-    RiZoeL8 = None
+    StarX8 = None
 
 if SESSION9:
-    RiZoeL9 = Client(name="SESSION9", api_id = API_ID, api_hash = API_HASH, session_string=SESSION9, plugins=dict(root="SpamX.module"))
+    StarX9 = Client(name="SESSION9", api_id = API_ID, api_hash = API_HASH, session_string=SESSION9, plugins=dict(root="SpamX.module"))
     print("Client 9 Found")
 else:
-    RiZoeL9 = None
+    StarX9 = None
 
 if SESSION10:
-    RiZoeL10 = Client(name="SESSION10", api_id = API_ID, api_hash = API_HASH, session_string=SESSION10, plugins=dict(root="SpamX.module"))
+    StarX10 = Client(name="SESSION10", api_id = API_ID, api_hash = API_HASH, session_string=SESSION10, plugins=dict(root="SpamX.module"))
     print("Client 10 Found")
 else:
-    RiZoeL10 = None
+    StarX10 = None
 
 if SESSION11:
-    RiZoeL11 = Client(name="SESSION11", api_id = API_ID, api_hash = API_HASH, session_string=SESSION11, plugins=dict(root="SpamX.module"))
+    StarX11 = Client(name="SESSION11", api_id = API_ID, api_hash = API_HASH, session_string=SESSION11, plugins=dict(root="SpamX.module"))
     print("Client 11 Found")
 else:
-    RiZoeL11 = None
+    StarX11 = None
 
 if SESSION12:
-    RiZoeL12 = Client(name="SESSION12", api_id = API_ID, api_hash = API_HASH, session_string=SESSION12, plugins=dict(root="SpamX.module"))
+    StarX12 = Client(name="SESSION12", api_id = API_ID, api_hash = API_HASH, session_string=SESSION12, plugins=dict(root="SpamX.module"))
     print("Client 12 Found")
 else:
-    RiZoeL12 = None
+    StarX12 = None
 
 if SESSION13:
-    RiZoeL13 = Client(name="SESSION13", api_id = API_ID, api_hash = API_HASH, session_string=SESSION13, plugins=dict(root="SpamX.module"))
+    StarX13 = Client(name="SESSION13", api_id = API_ID, api_hash = API_HASH, session_string=SESSION13, plugins=dict(root="SpamX.module"))
     print("Client 13 Found")
 else:
-    RiZoeL13 = None
+    StarX13 = None
 
 if SESSION14:
-    RiZoeL14 = Client(name="SESSION14", api_id = API_ID, api_hash = API_HASH, session_string=SESSION14, plugins=dict(root="SpamX.module"))
+    StarX14 = Client(name="SESSION14", api_id = API_ID, api_hash = API_HASH, session_string=SESSION14, plugins=dict(root="SpamX.module"))
     print("Client 14 Found")
 else:
-    RiZoeL14 = None
+    StarX14 = None
 
 if SESSION15:
-    RiZoeL15 = Client(name="SESSION15", api_id = API_ID, api_hash = API_HASH, session_string=SESSION15, plugins=dict(root="SpamX.module"))
+    StarX15 = Client(name="SESSION15", api_id = API_ID, api_hash = API_HASH, session_string=SESSION15, plugins=dict(root="SpamX.module"))
     print("Client 15 Found")
 else:
-    RiZoeL15 = None
+    StarX15 = None
 
 if SESSION16:
-    RiZoeL16 = Client(name="SESSION16", api_id = API_ID, api_hash = API_HASH, session_string=SESSION16, plugins=dict(root="SpamX.module"))
+    StarX16 = Client(name="SESSION16", api_id = API_ID, api_hash = API_HASH, session_string=SESSION16, plugins=dict(root="SpamX.module"))
     print("Client 16 Found")
 else:
-    RiZoeL16 = None
+    StarX16 = None
     
 if SESSION17:
-    RiZoeL17 = Client(name="SESSION17", api_id = API_ID, api_hash = API_HASH, session_string=SESSION17, plugins=dict(root="SpamX.module"))
+    StarX17 = Client(name="SESSION17", api_id = API_ID, api_hash = API_HASH, session_string=SESSION17, plugins=dict(root="SpamX.module"))
     print("Client 17 Found")
 else:
-    RiZoeL17 = None   
+    StarX17 = None   
     
 if SESSION18:
-    RiZoeL18 = Client(name="SESSION18", api_id = API_ID, api_hash = API_HASH, session_string=SESSION18, plugins=dict(root="SpamX.module"))
+    StarX18 = Client(name="SESSION18", api_id = API_ID, api_hash = API_HASH, session_string=SESSION18, plugins=dict(root="SpamX.module"))
     print("Client 18 Found")
 else:
-    RiZoeL18 = None     
+    StarX18 = None     
     
 if SESSION19:
-    RiZoeL19 = Client(name="SESSION19", api_id = API_ID, api_hash = API_HASH, session_string=SESSION19, plugins=dict(root="SpamX.module"))
+    StarX19 = Client(name="SESSION19", api_id = API_ID, api_hash = API_HASH, session_string=SESSION19, plugins=dict(root="SpamX.module"))
     print("Client 19 Found")
 else:
-    RiZoeL19 = None    
+    StarX19 = None    
 
 if SESSION20:
-    RiZoeL20 = Client(name="SESSION20", api_id = API_ID, api_hash = API_HASH, session_string=SESSION20, plugins=dict(root="SpamX.module"))
+    StarX20 = Client(name="SESSION20", api_id = API_ID, api_hash = API_HASH, session_string=SESSION20, plugins=dict(root="SpamX.module"))
     print("Client 20 Found")
 else:
-    RiZoeL20 = None
+    StarX20 = None
