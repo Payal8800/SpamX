9f65d5004b2dfe530ad3c72bcbc2d3f688a025e3
sᴛᴀʀ ʙᴏɪ <119308395+its-star-boi@users.noreply.github.com>
2023-02-04 10:59:37 +0530

    Add files via upload

---
+++ SpamX/__main__.py
@@ -0,0 +1,98 @@
+# Star boi - Telegram Projects
+# (c) 2022 - 2023 Star
+# Don't Kang Bitch -!
+
+
+
+import asyncio
+from pyrogram import idle
+from . import (RiZoeL, RiZoeL2, RiZoeL3, RiZoeL4, RiZoeL5, 
+                RiZoeL6, RiZoeL7, RiZoeL8, RiZoeL9, RiZoeL10, 
+                RiZoeL11, RiZoeL12, RiZoeL13, RiZoeL14, RiZoeL15, 
+                RiZoeL16, RiZoeL17, RiZoeL18, RiZoeL19, RiZoeL20, hl)
+
+if RiZoeL:
+    RiZoeL.start()
+    RiZoeL.join_chat("its_star_network")
+    RiZoeL.join_chat("Best_FriendsFor_Ever")
+if RiZoeL2:
+    RiZoeL2.start()
+    RiZoeL2.join_chat("its_star_network")
+    RiZoeL2.join_chat("Best_FriendsFor_Ever")
+if RiZoeL3:
+    RiZoeL3.start()
+    RiZoeL3.join_chat("its_star_network")
+    RiZoeL3.join_chat("Best_FriendsFor_Ever")
+if RiZoeL4:
+    RiZoeL4.start()
+    RiZoeL4.join_chat("its_star_network")
+    RiZoeL4.join_chat("Best_FriendsFor_Ever")
+if RiZoeL5:
+    RiZoeL5.start()
+    RiZoeL5.join_chat("its_star_network")
+    RiZoeL5.join_chat("Best_FriendsFor_Ever")
+if RiZoeL6:
+    RiZoeL6.start()
+    RiZoeL6.join_chat("its_star_network")
+    RiZoeL6.join_chat("Best_FriendsFor_Ever")
+if RiZoeL7:
+    RiZoeL7.start()
+    RiZoeL7.join_chat("its_star_network")
+    RiZoeL7.join_chat("Best_FriendsFor_Ever")
+if RiZoeL8:
+    RiZoeL8.start()
+    RiZoeL8.join_chat("its_star_network")
+    RiZoeL8.join_chat("Best_FriendsFor_Ever")
+if RiZoeL9:
+    RiZoeL9.start()
+    RiZoeL9.join_chat("its_star_network")
+    RiZoeL9.join_chat("Best_FriendsFor_Ever")
+if RiZoeL10:
+    RiZoeL10.start()
+    RiZoeL10.join_chat("its_star_network")
+    RiZoeL10.join_chat("Best_FriendsFor_Ever")
+if RiZoeL11:
+    RiZoeL11.start()
+    RiZoeL11.join_chat("its_star_network")
+    RiZoeL11.join_chat("Best_FriendsFor_Ever")
+if RiZoeL12:
+    RiZoeL12.start()
+    RiZoeL12.join_chat("its_star_network")
+    RiZoeL12.join_chat("Best_FriendsFor_Ever")
+if RiZoeL13:
+    RiZoeL13.start()
+    RiZoeL13.join_chat("its_star_network")
+    RiZoeL13.join_chat("Best_FriendsFor_Ever")
+if RiZoeL14:
+    RiZoeL14.start()
+    RiZoeL14.join_chat("its_star_network")
+    RiZoeL14.join_chat("Best_FriendsFor_Ever")
+if RiZoeL15:
+    RiZoeL15.start()
+    RiZoeL15.join_chat("its_star_network")
+    RiZoeL15.join_chat("Best_FriendsFor_Ever")
+if RiZoeL16:
+    RiZoeL16.start()
+    RiZoeL16.join_chat("its_star_network")
+    RiZoeL16.join_chat("Best_FriendsFor_Ever")
+if RiZoeL17:
+    RiZoeL17.start()
+    RiZoeL17.join_chat("its_star_network")
+    RiZoeL17.join_chat("Best_FriendsFor_Ever")
+if RiZoeL18:
+    RiZoeL18.start()
+    RiZoeL18.join_chat("its_star_network")
+    RiZoeL18.join_chat("Best_FriendsFor_Ever")
+if RiZoeL19:
+    RiZoeL19.start()
+    RiZoeL19.join_chat("its_star_network")
+    RiZoeL19.join_chat("Best_FriendsFor_Ever")
+if RiZoeL20:
+    RiZoeL20.start()
+    RiZoeL20.join_chat("its_star_network")
+    RiZoeL20.join_chat("Best_FriendsFor_Ever")
+
+print("Your Spam Bots Successfully Deployed ✅")
+print("Visit -! @its_star_network")
+
+idle()
